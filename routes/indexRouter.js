2b4f521483ea15e3a8f3476cddebbb6e4275e987
Dukyoon Im <DUKY8N@gmail.com>
2023-12-02 17:00:38 +0900

    루트 라우팅, a 태그 경로 수정

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const indexController = require("../controller/indexController");
 
 
-router.get("/", indexController.getMyGradesPage);
+router.get("/", (req, res) => res.redirect("/myGrades"));
 router.get("/myGrades", indexController.getMyGradesPage);
 router.get("/myFriends", indexController.getMyFriendsPage);
 router.get("/login", indexController.getLogInPage);
