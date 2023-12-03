9bfa02d72c1a3b87f460dd518745fb46fd080466
Dukyoon Im <DUKY8N@gmail.com>
2023-12-04 05:19:40 +0900

    로그인 여부 리디렉션 기능 추가

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,9 +7,23 @@ exports.isLoginStatus = (req, res, next) => {
   return next(createError(401, "not_login_status_access_error"));
 };
 
+exports.isLoginStatusOrRedirect = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/logIn');
+};
+
 exports.isLogoutStatus = (req, res, next) => {
   if (!req.isAuthenticated()) {
     return next();
   }
   return next(createError(403, "not_logout_status_access_error"));
+};
+
+exports.isLogoutStatusOrRedirect = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const indexController = require("../controller/indexController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 
 router.get("/", (req, res) => res.redirect("/myGrades"));
-router.get("/myGrades", indexController.getMyGradesPage);
-router.get("/myFriends", indexController.getMyFriendsPage);
-router.get("/login", indexController.getLogInPage);
-router.get("/signup", indexController.getSignUpPage);
-router.get("/accountSettings", indexController.getAccountSettingsPage);
+router.get("/myGrades", authMiddleware.isLoginStatusOrRedirect, indexController.getMyGradesPage);
+router.get("/myFriends", authMiddleware.isLoginStatusOrRedirect, indexController.getMyFriendsPage);
+router.get("/login", authMiddleware.isLogoutStatusOrRedirect, indexController.getLogInPage);
+router.get("/signup", authMiddleware.isLogoutStatusOrRedirect, indexController.getSignUpPage);
+router.get("/accountSettings", authMiddleware.isLoginStatusOrRedirect, indexController.getAccountSettingsPage);
 
 module.exports = router;
