e5e5332e706deaa211976599c24535b9a16e48c9
Dukyoon Im <DUKY8N@gmail.com>
2023-12-13 21:48:29 +0900

    myGrade 학기 성적 탐색 기능 구현

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,12 +1,44 @@
 const createError = require("http-errors");
+const usersController = require("../controller/usersController");
+const subjectsModel = require("../model/subjectsModel");
+
+function getYearAndHalf() {
+    let today = new Date();
+
+    let year = today.getFullYear(); // 년도
+    let month = today.getMonth() + 1; // 월 (0부터 시작하므로 1을 더함)
+    let half;
+
+    // 1월부터 6월은 1, 7월부터 12월은 2
+    if (month >= 1 && month <= 6) {
+        half = 1;
+    } else {
+        half = 2;
+    }
+
+    return year + '-' + half; // "년도-반기" 형식의 문자열 반환
+}
 
 exports.getMyGradesPage = async (req, res, next) => {
     const articles = [];
-    for (let i = 1; i <= 18; i++) {
-        articles.push({ content: '내용 ' + i });
-    }
+    let userName = "";
+    let date = req.params.date || getYearAndHalf();
+    const dateScores = await subjectsModel.dateScore(date, req.user);
 
-    res.render('myGrades', { articles: articles }); 
+    for (let i = 1; i <= 18; i++) {
+        articles.push({ content: date + i });
+    }
+    try {
+        userName = await usersController.getUserName(req.user);
+    } catch (error) {
+        console.error(error);
+        return next(createError(500, "Server Error"));
+    }
+    res.render('myGrades', {
+        scores: dateScores,
+        userName: userName,
+        date: date
+    }); 
 };
 
 exports.getMyFriendsPage = async (req, res, next) => {
---
+++ public/javascripts/myGrades.js
@@ -0,0 +1,43 @@
+let date;
+let formattedDate;
+document.addEventListener('DOMContentLoaded', (event) => {
+    date = document.body.dataset.date;
+    formattedDate = convertToGradeFormat(date);
+    document.getElementById('date').innerText = formattedDate;
+});
+
+function convertToGradeFormat(str) {
+    let parts = str.split('-'); // 문자열을 '-' 기준으로 분리
+    let year = parts[0]; // 첫 번째 부분은 년도
+    let semester = parts[1]; // 두 번째 부분은 학기
+
+    return year + '년도 ' + semester + '학기 성적';
+}
+
+function navigateDate(direction) {
+    console.log(direction);
+    console.log(date);
+
+    let parts = date.split('-');
+    let year = parseInt(parts[0]);
+    let semester = parseInt(parts[1]);
+
+    if (direction === 'next') {
+        if (semester === 1) {
+            semester = 2;
+        } else {
+            year += 1;
+            semester = 1;
+        }
+    } else if (direction === 'previous') {
+        if (semester === 2) {
+            semester = 1;
+        } else {
+            year -= 1;
+            semester = 2;
+        }
+    }
+
+    window.location.href = `/myGrades/${year + '-' + semester}`
+}
+//TODO 연도 이동 버튼 누를 시 연도 변경
