2d067f4589328dcffe58c87611c03550307c8ba9
DESKTOP-PR39U5I\82106 <smmaxcho@naver.com>
2023-12-14 13:56:18 +0900

    작업중

--- controller/friendsController.js
+++ controller/friendsController.js
@@ -24,13 +24,14 @@ exports.addFriend = async function (req,res,next) {
 exports.deleteFriend = async function (req,res,next) {
   try{
     //필수 입력 필드 검사
-    const f_id = req.body;
-    if(!f_id){
-      console.log(f_id);
+    const {id, friend_id} = req.body;
+    console.log(id, friend_id)
+    if(!friend_id){
+      console.log(friend_id);
       return next(createError(400, "Missing required fields"));
     }
 
-    await friendsModel.deleteFriend({f_id});
+    await friendsModel.deleteFriend(friend_id);
     return res.status(200).json({ message: "Delete Friend!" });
   }catch (error) {
     next(error);
@@ -40,23 +41,37 @@ exports.deleteFriend = async function (req,res,next) {
 exports.compareScore = async function (req,res,next) {
   try{
     //필수 입력 필드 검사
-    const {id,f_id} = req.body;
-    if(!id || !f_id){
-      console.log(id, f_id);
+    const {id,friend_id} = req.body;
+    const result = await friendsModel.compareScore(req.subject_name, student_id, friendstudent_id);
+    
+    if (result && result.length > 0) {
+      res.status(200).json({ success: true, result });
+    } else {
+      res.status(404).json({ success: false, message: 'Student scores not found' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+//내기준 친구 성적 비교, 친구기준 성적 비교
+      
+    /*if(!id || !friend_id){
+      console.log(id, friend_id);
       return next(createError(400, "Missing required fields"));
     }
 
     //친구 성적 비교, 친구가 있는지, 
-
-    if(f_id.length > 0){
+    if(friend_id.length > 0){
       return res.status(200).json({ message: "Found Friend!" });
     } else{
       return res.status(404).json({ message: "Not found Friend!" });
     }
 
-    await friendsModel.compareScore({f_id});
-    return res.status(200).json({ message: "!" });
+    await friendsModel.compareScore(friend_id);
+    return res.status(200).json({ message: "Compare Score!" });
   }catch (error) {
     next(error);
   }
-};
+};*/
--- model/friendsModel.js
+++ model/friendsModel.js
@@ -1,22 +1,65 @@
 const { poolPromise } = require("./index");
 
-exports.addFriend = async function (id,f_id) {
+exports.addFriend = async function (id,friend_id) {
   const pool = await poolPromise;
-  await pool.query`INSERT INTO Friend (student_id,friend_student_id) VALUES(${id},${f_id})`;
+  const {recordset} = 
+  await pool.query`INSERT INTO Friend (student_id,friend_student_id) VALUES(${id},${friend_id})`;
+  return recordset
 };
 
 
-exports.compareScore = async function (id,f_id) {
+exports.compareScore = async function (id,friend_id) {
   const pool = await poolPromise;
-  await pool.query`SELECT COALESCE(StudentScore.subject_name,'') AS subject_name, COALESCE(StudentScore.grade,'') AS student_grade, COALESCE(FriendScore.grade,'') AS friend_grade FROM Friend LEFT JOIN Score AS StudentScore ON Friend.student_id = StudentScore.student_id 
-  LEFT JOIN Score AS FriendScore ON Friend.friend_student_id = FriendScore.student_id
-  WHERE 
-    (Friend.student_id = ${id} AND Friend.friend_student_id = ${f_id} )
-    OR (Friend.friend_student_id = ${f_id} AND Friend.student_id = ${id} );`;
+  const { recordset } = 
+  await pool.query`SELECT COALESCE(userStudent.subject_code, friendStudent.subject_code) AS subject_code,
+  COALESCE(userStudent.subject_name, friendStudent.subject_name) AS subject_name,
+   userStudent.grade AS userStudentGrade, 
+   friendStudent.grade AS friendStudentGrade
+FROM (SELECT * FROM Score WHERE student_id = ${id} ) AS userStudent
+FULL OUTER JOIN (SELECT * FROM Score WHERE student_id = ${friend_id}) AS friendStudent
+ON userStudent.subject_code = friendStudent.subject_code;`;
+return recordset;
+};
+
+exports.mycompareScore = async function (id,friend_id) {
+  const pool = await poolPromise;
+  const { recordset } = 
+  await pool.query`SELECT 
+  COALESCE(userStudent.subject_code, friendStudent.subject_code) AS subject_code,
+  COALESCE(userStudent.subject_name, friendStudent.subject_name) AS subject_name,
+  userStudent.grade AS userStudentGrade, 
+  friendStudent.grade AS friendStudentGrade
+FROM 
+  (SELECT subject_code, subject_name, grade FROM Score WHERE student_id = ${id}) AS userStudent
+LEFT OUTER JOIN 
+  (SELECT subject_code, subject_name, grade FROM Score WHERE student_id = ${friend_id}) AS friendStudent
+ON 
+  userStudent.subject_code = friendStudent.subject_code;`;
+return recordset;
+};
+
+exports.friendcompareScore = async function (id,friend_id) {
+  const pool = await poolPromise;
+  const { recordset } = 
+  await pool.query`SELECT 
+  COALESCE(userStudent.subject_code, friendStudent.subject_code) AS subject_code,
+  COALESCE(userStudent.subject_name, friendStudent.subject_name) AS subject_name,
+  userStudent.grade AS userStudentGrade, 
+  friendStudent.grade AS friendStudentGrade
+FROM 
+  (SELECT subject_code, subject_name, grade FROM Score WHERE student_id = ${friend_id}) AS friendStudent
+RIGHT OUTER JOIN 
+  (SELECT subject_code, subject_name, grade FROM Score WHERE student_id = ${id}) AS userStudent
+ON 
+  userStudent.subject_code = friendStudent.subject_code;`;
+return recordset;
 };
 
 
-exports.deleteFriend = async function (id,f_id) {
+
+exports.deleteFriend = async function (id,friend_id) {
   const pool = await poolPromise;
-  await pool.query`DELETE FROM Friend WHERE (student_id = ${id} AND friend_student_id = ${f_id});`;
+  const {recordset} = 
+  await pool.query`DELETE FROM Friend WHERE (student_id = ${id} AND friend_student_id = ${friend_id});`;
+  return recordset;
 };
